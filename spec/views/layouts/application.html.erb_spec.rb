0738c91dd242ed8c0d85827baae97ed611634a43
maru <marikorin815@gmail.com>
2019-07-15 01:17:01 +0900

    Fix test

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Test rootpath', type: :view do
-    
   it 'renders a link to home' do
-     render
-     assert_select 'a[href=?]', root_path
+    render template: 'home/index', layout: 'layouts/application'
+    assert_select 'a[href=?]', root_path
   end
 end
