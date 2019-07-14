d63f91249b7f229efd291e977b12ac4d6bad76d9
kokoriru <ogihara.yuka.b@gmail.com>
2019-07-15 01:59:05 +0900

    Fix specs to pets

--- spec/factories/pets.rb
+++ spec/factories/pets.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     association :user
     name { 'MyString' }
     race { 'MyString' }
+    sex { true }
     birthday { '2000-05-05' }
     greeted_on { '2000-06-11' }
     note { 'MyText' }
