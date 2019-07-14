0b80a2a58709314f6e9063ce5af98f3239ec1f0b
kokoriru <ogihara.yuka.b@gmail.com>
2019-07-15 01:56:36 +0900

    Execute `rails db:migrate:reset`

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2019_07_06_140647) do
     t.integer "user_id"
     t.string "name"
     t.string "race"
+    t.boolean "sex"
     t.date "birthday"
     t.date "greeted_on"
     t.text "note"
