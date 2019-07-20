88ee9ba538f3fb4ef62fe1935bf812a553633496
Chi-rr <chi.r.r.333@gmail.com>
2019-07-21 05:06:59 +0900

    rubocopによるインデントの修正

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'home/index.html.erb', type: :view do
   it 'renders a link to users' do
-      render
-      assert_select "a[href=?]" , users_path
-    end
+    render
+    assert_select 'a[href=?]', users_path
+  end
 end
