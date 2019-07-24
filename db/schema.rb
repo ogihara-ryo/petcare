276ffe7c4d4b464421943f8c67d07b85d991f0ad
Chi-rr <chi.r.r.333@gmail.com>
2019-07-24 13:04:35 +0900

    execute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_192233) do
+ActiveRecord::Schema.define(version: 2019_07_24_022257) do
+
+  create_table "excretes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pets", force: :cascade do |t|
     t.integer "user_id"
