74cba6a3fe92ce328908acba193c97c097c920e0
Chi-rr <chi.r.r.333@gmail.com>
2019-07-26 06:04:23 +0900

    Add relation Pet and  Excrete

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,6 @@
 class Pet < ApplicationRecord
   belongs_to :user
   has_many :physicals, dependent: :destroy
-
+  has_many :excretes, dependent: :destroy
   enum sex: { '不明' => 0, 'オス' => 1, 'メス' => 2, '適用不能' => 9 }
 end
