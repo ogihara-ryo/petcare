ba5f5ef7f3857d3b1f6933f688ecd3ee2e4e6244
Chi-rr <chi.r.r.333@gmail.com>
2019-08-19 15:09:11 +0900

    Add excrete's specs2

--- spec/models/excrete_spec.rb
+++ spec/models/excrete_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Excrete, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.belong_to' do
+    let(:excrete) { FactryBot.create(:excrete, pet: pet) }
+
+    describe 'pet' do
+      let(:pet){ FactryBot.create(:pet, user: FactryBot.create(:user), name: 'spica') }
+      before { excrete.pet = pet }
+      it { expect(excrete.pet).to eq pet }
+    end
+  end
 end
