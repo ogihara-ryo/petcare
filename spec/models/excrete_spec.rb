2aa9832d17beb405c91cdc6a7a9cf54eeaa0df9d
Chi-rr <chi.r.r.333@gmail.com>
2019-08-22 12:42:06 +0900

    Correct typos

--- spec/models/excrete_spec.rb
+++ spec/models/excrete_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Excrete, type: :model do
   describe '.belong_to' do
-    let(:excrete) { FactryBot.create(:excrete, pet: pet) }
+    let(:excrete) { FactoryBot.create(:excrete, pet: pet) }
 
     describe 'pet' do
-      let(:pet){ FactryBot.create(:pet, user: FactryBot.create(:user), name: 'spica') }
+      let(:pet) { FactoryBot.create(:pet, user: FactoryBot.create(:user), name: 'spica') }
       before { excrete.pet = pet }
       it { expect(excrete.pet).to eq pet }
     end
