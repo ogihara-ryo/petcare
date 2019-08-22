14b7eb78c7c161028bd948728553d0b6b880b935
Chi-rr <chi.r.r.333@gmail.com>
2019-08-22 12:54:24 +0900

    Correct excrete's update spec

--- spec/views/excretes/update_spec.rb
+++ spec/views/excretes/update_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature 'pet/1/excretes/edit/1', typpe: :system do
-  givin!(:pet) { FactoryBot.create(:pet, user: FactoryBot.create(:user), name: 'アルク') }
+RSpec.feature 'pets/1/excretes/edit/1', typpe: :system do
+  given!(:pet) { FactoryBot.create(:pet, user: FactoryBot.create(:user), name: 'アルク') }
   given!(:excrete1) { FactoryBot.create(:excrete, pet: pet) }
 
   background do
@@ -17,11 +17,11 @@ RSpec.feature 'pet/1/excretes/edit/1', typpe: :system do
     end
 
     scenario 'it is update' do
-      expect(current_path).to eq pet_excretes_path(pet)
+      expect(current_path).to eq pet_excrete_path(pet, excrete1)
       expect(page).to have_text 'Excrete was successfully updated.'
       expect(page).to have_text 'アルク'
       expect(page).to have_text '順調に成長中!'
-      expect(page).to have_text '2019/08/18 10:18:45'
+      expect(page).to have_text '2019-08-18 10:18:45'
     end
   end
 end
