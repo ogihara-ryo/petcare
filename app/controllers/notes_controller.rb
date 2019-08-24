fb26cef1e12b79e3c0cd1a1e2af3ca9995f588f3
Chi-rr <chi.r.r.333@gmail.com>
2019-08-24 17:55:05 +0900

    Add note's actions

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,64 @@
+class NotesController < ApplicationController
+  before_action :set_pet
+  before_action :set_note, only: %i[show edit update destroy]
+
+  def index
+    @notes = Note.where(pet: @pet).order(date: :desc)
+  end
+
+  def show; end
+
+  def new
+    @note = Note.new
+  end
+
+  def edit; end
+
+  def create
+    @note = Note.new(note_params)
+
+    respond_to do |format|
+      if @note.save
+        format.html { redirest_to pet_notes_path, notice: 'Note was successfully created.' }
+        format.json { render :show, status: :created, location: @note }
+      else
+        format.html { render :new }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @note.update(note_params)
+        format.html { redirest_to pet_notes_path, notice: 'Physical was successfully updated.' }
+        format.json { render :show, status: :ok, location: @note }
+      else
+        format.html { render :edit }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @note.destroy
+    respond_to do |format|
+      format.html { redirest_to pet_notes_path, notice: 'Note was successfully destroyed.' }
+      format.json { head :mp_content }
+    end
+  end
+
+  private
+
+  def set_pet
+    @pet = Pet.find(params[:pet_id])
+  end
+
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:pet_id, :body, :datetime)
+  end
+end
