3165f572e987c47bfd701cae74fe1586fd98bdd4
maru <marikorin815@gmail.com>
2019-08-25 16:49:48 +0900

    Fix meal's table

--- db/migrate/20190720052142_create_meals.rb
+++ db/migrate/20190720052142_create_meals.rb
@@ -1,6 +1,7 @@
 class CreateMeals < ActiveRecord::Migration[5.2]
   def change
     create_table :meals do |t|
+      t.belongs_to :pet, foreign_key: true
       t.string :food
       t.text :quantity
       t.text :note
